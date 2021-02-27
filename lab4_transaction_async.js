c956700426c018b4329d883a2a9b64318728847c
eoinco <eoin.connolly@gmail.com>
2021-02-27 16:18:09 +0000

    about ot add private keys

--- lab4_transaction_async.js
+++ lab4_transaction_async.js
@@ -1,1 +1,26 @@
+const Tx = require('ethereumjs-tx').Transaction
+
 const Web3 = require('web3')
+
+const web3 = new Web3("https://ropsten.infura.io/v3/1e86eba9862b4b78b37cfaf9675ffcad")
+
+const account1 = "0x1cF88d2952A6d7FB2f6F9E76BEB2A7904c9504BA"  // this is the address of account 1 - this guy has all the MONEH
+const account2 = "0x3d6d5A49d89eC0760E3607b287aE7693FA8D0c9A"  // this is the address of account 2 - this guy has very little MONEH
+
+const privateKey1 = Buffer.from('...', 'hex')
+const privateKey2 = Buffer.from('...', 'hex')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
