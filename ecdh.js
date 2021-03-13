c1924792ac5ec6bc472a3241b40d1a48133afb8e
eoinco <eoin.connolly@gmail.com>
2021-03-13 17:24:45 +0000

    created alice's keypair

--- ecdh.js
+++ ecdh.js
@@ -3,10 +3,14 @@ var sodium = require('sodium-native')
 // ECDH
 // x25519
 
-
 // Create Alice keypair
 var AlicePublicKey = sodium.sodium_malloc(sodium.crypto_box_PUBLICKEYBYTES)
 var AlicePrivateKey = sodium.sodium_malloc(sodium.crypto_box_SECRETKEYBYTES)
+
+sodium.crypto_box_keypair(AlicePublicKey, AlicePrivateKey)
+
+console.log(`Alice ${sodium.crypto_box_PUBLICKEYBYTES}-byte public key is: ${AlicePublicKey.toString('hex')}`)
+console.log(`Alice ${sodium.crypto_box_SECRETKEYBYTES}-byte private key is: ${AlicePrivateKey.toString('hex')}`)
 
 // Create Bob keypair
 
